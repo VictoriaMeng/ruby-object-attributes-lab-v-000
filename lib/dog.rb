3fd9d25a997dc43d4904cbba29f9b13c1dc03aa9
Victoria Meng <nights1ststar@protonmail.com>
2017-12-28 00:02:46 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,15 @@
 class Dog
-  attr_accessor :name, :breed
-
+  
   def initialize(name, breed)
     @name = name
     @breed = breed
+  end
+
+  def name
+    @name
+  end
+
+  def name=(name)
+    @name = name
   end
 end
